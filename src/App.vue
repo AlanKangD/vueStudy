b0a239e904fafe971a862e85f8feb96cc2d05291
alan <kno12350@gmail.com>
2022-11-01 08:57:51 +0900

    주석 설명 추가됨

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
     <a v-for="menu in menus" :key="menu"> {{ menu }} </a>
     
   </div>
-
+  <!--produnct.index 는 해당 반복문의 index 번호라고 생각하면 됩니다. 그래서 유니크한 키를 그 영역에 삽입해준다고 생각하면됨-->
   <div v-for="produnct in productJson" :key="produnct.index">
-    <p>글번호 : {{produnct.index}}</p>
+    <!-- 반복문이 돌 경ㅇ우 각각의 한영역을 produnct 에 값을 넣어서 해당 json 데이터의 key값을 넣어주면 데이터를 잘 출력합니다, -->
+    <p>글번호 : {{produnct.index}}</p> 
     <h4>{{ produnct.porduct }}</h4>
     <p>{{ produnct.price }} 만원</p>
   </div> 
